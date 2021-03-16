e0ff70b337dbc7d871b97c9488eb19ab2594e379
lior84 <liorhar84@gmail.com>
2021-03-16 21:14:17 +0200

    part 2- q1,q3 are done

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -4,80 +4,47 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 //#1
-// const countVowels = (str: string): number => {
-//     return str.match(/[aAeEiIoOuU]/g).length;
-// }
-
-// console.log(countVowels("This is SOME Text"));
+const countVowels = (str: string): number => {
+    return R.match(/[aAeEiIoOuU]/g, str).length;
+}
 
 //#3
-// let isBalancedParenthesis = (str: string, parenthesesLeft: string, parenthesesRight: string,) => {
-//     let wrongArrangement = false;
-//     let sumOfOarenthesesThatAreEqual = !str.split('').reduce((uptoPrevChar: number, thisChar: string) => 
-//     {
-//         if(uptoPrevChar < 0)
-//             wrongArrangement = true;
-//         else 
-//         {
-//             if(thisChar === parenthesesLeft)
-//                 return ++uptoPrevChar;
-//             else if (thisChar === parenthesesRight) 
-//                 return --uptoPrevChar;
-//         }
-//         return uptoPrevChar 
-//     }, 0);
-//     return wrongArrangement ? false : sumOfOarenthesesThatAreEqual;
-// }
-
-// let isPaired = (str: string) =>{
-//     return isBalancedParenthesis(str, "(", ")") && isBalancedParenthesis(str, "[", "]") && isBalancedParenthesis(str, "{", "}");
-// }
-
 let isPaired = (str: string) =>{
     let stack: string[] = [];
     let flag = true;
     let sumOfOarenthesesThatAreEqual = !str.split('').reduce((uptoPrevChar: number, thisChar: string) => 
     {
-            if(thisChar === "("){
-                stack = R.insert(stack.length, "(", stack)
-            }
-            else if(thisChar === "["){
-                stack = R.insert(stack.length, "[", stack)
-            }
-            else if(thisChar === "{"){
-                stack = R.insert(stack.length, "{", stack)
-            }
+            if(thisChar === "("){ stack = R.insert(stack.length, "(", stack) }
+            else if(thisChar === "["){ stack = R.insert(stack.length, "[", stack) }
+            else if(thisChar === "{"){ stack = R.insert(stack.length, "{", stack) }
             else if(thisChar === ")"){
-                if(stack[stack.length-1] === "("){
+                if(stack[stack.length-1] === "(")
                     stack = R.remove(stack.length-1, 1, stack)
-                }
-                else{
+                else
                     flag = false;
-                }
             }
             else if(thisChar === "]"){
-                if(stack[stack.length-1] === "["){
+                if(stack[stack.length-1] === "[")
                     stack = R.remove(stack.length-1, 1, stack)
-                }
-                else{
+                else
                     flag = false;
-                }
             }
             else if (thisChar === "}"){
-                if(stack[stack.length-1] === "{"){
+                if(stack[stack.length-1] === "{")
                     stack = R.remove(stack.length-1, 1, stack)
-                }
-                else{
+                else
                     flag = false;
-                }
             }
         return uptoPrevChar 
-}, 0);
+    }, 0);
+
     if(stack.length != 0)
         flag = false
     console.log(stack);
     return flag
 }
 
-
-console.log(isPaired("Thi{{{{{}}}}}"))
+//tests #1
+console.log(countVowels("This is SOME Texti"));
+//tests #3
+//console.log(isPaired("Thi{{{[{{}}}}}"))
