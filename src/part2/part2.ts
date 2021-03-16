07659cde0b9ded49c0da71551c7829ecaa006715
lior84 <liorhar84@gmail.com>
2021-03-16 21:04:48 +0200

    Updated question 3

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,34 +1,83 @@
-import { slice } from "ramda";
+import { insert, slice } from "ramda";
+import * as R from "ramda";
 
+const stringToArray = R.split("");
 
 //#1
-const countVowels = (str: string): number => {
-    return str.match(/[aAeEiIoOuU]/g).length;
-}
+// const countVowels = (str: string): number => {
+//     return str.match(/[aAeEiIoOuU]/g).length;
+// }
 
-console.log(countVowels("This is SOME Text"));
+// console.log(countVowels("This is SOME Text"));
 
 //#3
-let isBalancedParenthesis = (str: string, parenthesesLeft: string, parenthesesRight: string,) => {
-    let wrongArrangement = false;
-    let sumOfOarenthesesThatAreEqual = !str.split('').reduce((uptoPrevChar: number, thisChar: string) => 
-    {
-        if(uptoPrevChar < 0)
-            wrongArrangement = true;
-        else 
-        {
-            if(thisChar === parenthesesLeft)
-                return ++uptoPrevChar;
-            else if (thisChar === parenthesesRight) 
-                return --uptoPrevChar;
-        }
-        return uptoPrevChar 
-    }, 0);
-    return wrongArrangement ? false : sumOfOarenthesesThatAreEqual;
-}
+// let isBalancedParenthesis = (str: string, parenthesesLeft: string, parenthesesRight: string,) => {
+//     let wrongArrangement = false;
+//     let sumOfOarenthesesThatAreEqual = !str.split('').reduce((uptoPrevChar: number, thisChar: string) => 
+//     {
+//         if(uptoPrevChar < 0)
+//             wrongArrangement = true;
+//         else 
+//         {
+//             if(thisChar === parenthesesLeft)
+//                 return ++uptoPrevChar;
+//             else if (thisChar === parenthesesRight) 
+//                 return --uptoPrevChar;
+//         }
+//         return uptoPrevChar 
+//     }, 0);
+//     return wrongArrangement ? false : sumOfOarenthesesThatAreEqual;
+// }
+
+// let isPaired = (str: string) =>{
+//     return isBalancedParenthesis(str, "(", ")") && isBalancedParenthesis(str, "[", "]") && isBalancedParenthesis(str, "{", "}");
+// }
 
 let isPaired = (str: string) =>{
-    return isBalancedParenthesis(str, "(", ")") && isBalancedParenthesis(str, "[", "]") && isBalancedParenthesis(str, "{", "}");
+    let stack: string[] = [];
+    let flag = true;
+    let sumOfOarenthesesThatAreEqual = !str.split('').reduce((uptoPrevChar: number, thisChar: string) => 
+    {
+            if(thisChar === "("){
+                stack = R.insert(stack.length, "(", stack)
+            }
+            else if(thisChar === "["){
+                stack = R.insert(stack.length, "[", stack)
+            }
+            else if(thisChar === "{"){
+                stack = R.insert(stack.length, "{", stack)
+            }
+            else if(thisChar === ")"){
+                if(stack[stack.length-1] === "("){
+                    stack = R.remove(stack.length-1, 1, stack)
+                }
+                else{
+                    flag = false;
+                }
+            }
+            else if(thisChar === "]"){
+                if(stack[stack.length-1] === "["){
+                    stack = R.remove(stack.length-1, 1, stack)
+                }
+                else{
+                    flag = false;
+                }
+            }
+            else if (thisChar === "}"){
+                if(stack[stack.length-1] === "{"){
+                    stack = R.remove(stack.length-1, 1, stack)
+                }
+                else{
+                    flag = false;
+                }
+            }
+        return uptoPrevChar 
+}, 0);
+    if(stack.length != 0)
+        flag = false
+    console.log(stack);
+    return flag
 }
 
-console.log(isPaired("This is ]some[ (text)"))
+
+console.log(isPaired("Thi{{{{{}}}}}"))
